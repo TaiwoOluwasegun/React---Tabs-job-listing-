d8bc77392a17518c54fe49d67420afdf4d45d3d6
Taiwo Segun <Taiwooluwasegun02@gmail.com>
2021-03-09 05:53:26 +0100

    display fetched data's first value layout

--- src/App.js
+++ src/App.js
@@ -27,7 +27,40 @@ function App() {
     )
   }
 
-  return <h2>tabs project setup</h2>
+  const { company, dates, duties, title } = jobs[value]
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>Experience</h2>
+        <div className="underline" />
+      </div>
+
+      <div className="jobs-center">
+        <article className="job-info">
+          <h3>
+            {title}
+          </h3>
+          <h4>
+            {company}
+          </h4>
+          <p className="job-date">
+            {dates}
+          </p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon" />
+                <p>
+                  {duty}
+                </p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default App
